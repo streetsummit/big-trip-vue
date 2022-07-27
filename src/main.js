6c3e7cfe98bf5ddbc051daa38a35cf83ea0ce960
Elizaveta <streetsummit@mail.ru>
2022-07-27 21:42:10 +0300

    feature: vuex => pinia

--- src/main.js
+++ src/main.js
@@ -1,23 +1,21 @@
 import { createApp } from 'vue';
 import App from './App';
+import { createPinia } from 'pinia';
 import router from './router';
-import store from './store';
 import directives from '@/directives';
 import PrimeVue from 'primevue/config';
 import '@/assets/css/primevue/MyTheme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 
-
-
-
 const app = createApp(App);
+const pinia = createPinia();
 
 directives.forEach(directive => {
 	app.directive(directive.name, directive)
 });
 
-app.use(store)
+app.use(pinia)
 	.use(router)
 	.use(PrimeVue)
 	.mount('#app');
---
+++ src/stores/CounterStore.js
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('CounterStore', {
+  state: () => ({ count: 0 }),
+  getters: {
+    double: state => state.count * 2,
+  },
+  actions: {
+    increment() {
+      this.count++
+    },
+  },
+})
