fbb71be0b24cbed290dc4795e0f66edb6b835bb2
Elizaveta <streetsummit@mail.ru>
2022-09-07 18:17:29 +0300

    cleanup

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,12 +1,1 @@
-const SYMBOLS =
-	'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-export const makeId = (idLength = 6) => {
-	let id = '';
-	for (let i = 0; i < idLength; i++) {
-		id += SYMBOLS.charAt(Math.floor(Math.random() * SYMBOLS.length));
-	}
-	return id;
-};
-
 export const capitalizeFirstLetter = (str) => str.charAt(0).toUpperCase() + str.slice(1);
