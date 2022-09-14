ef15c58bbabe98d481866367846b5a238e698bb5
Elizaveta <streetsummit@mail.ru>
2022-09-14 14:29:17 +0300

    feature: change pictures src

--- src/stores/DestinationsStore.js
+++ src/stores/DestinationsStore.js
@@ -15,7 +15,17 @@ export const useDestinationsStore = defineStore('destinationsStore', {
 		async fetchDestinations() {
 			try {
 				const response = await PointService.getDestinations();
-				this.destinationsData = response.data;
+				this.destinationsData = response.data.map((el) => {
+					return {
+						...el,
+						pictures: el.pictures.map((pic) => {
+							return {
+								...pic,
+								src: `https://loremflickr.com/300/200/city,${el.name}?random=${Math.random()}`,
+							};
+						}),
+					};
+				});
 			} catch (e) {
 				console.log(e);
 			}
